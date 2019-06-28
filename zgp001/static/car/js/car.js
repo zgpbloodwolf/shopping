cfb8b37bf5fb8b094b4094f89187a4701788c900
zgpbloodwolf <15770085681@163.com>
2019-06-28 12:43:12 +0800

    购物车

--- zgp001/static/car/js/car.js
+++ zgp001/static/car/js/car.js
@@ -1,9 +1,66 @@
-function radib() {
-    if($('input:radio:checked').val()=="checked")
-    {
+function addchange(idis){
+    fetch('/addchange'+'?car_id='+idis).then(function (response){return response.json()}).then(function (data) {
+        var a = data['data']
+        var b = data['danjia']
+        var c = data["zj"]
+        if(a == 'err')
+        {
+        }
+        else
+        {
+            $('#'+idis).text(a)
+            $('#'+idis+'jiage').text('￥'+b)
+            $('#jiesuanmoney').text('￥'+c)
+        }
+   })
+ }
+function jianchange(idis){
+    fetch('/jianchange'+'?car_id='+idis).then(function (response){return response.json()}).then(function (data) {
+        var a=data['data']
+        var b = data['danjia']
+        var c = data["zj"]
+        if( a == 0)
+        {
+            window.location.href="http://127.0.0.1:8000/car/"
 
-    }
+        }
+        else
+        {
+            $('#'+idis).text(a)
+            $('#'+idis+'jiage').text('￥'+b)
+            $('#jiesuanmoney').text('￥'+c)
+        }
+   })
+ }
+ function chosenco(coid) {
+     fetch('/chosenchange'+'?co_id='+coid).then(function (response){return response.json()}).then(function (data) {
+         var a = data['data']
+         window.location.href="http://127.0.0.1:8000/car/"
+            })
+ }
+ function jiesuan() {
+    var f = confirm('是否确认下单')
+     if(f){
+         fetch('/jiesuan').then(response => response.json()).then(data => {
+             window.location.href="http://127.0.0.1:8000/car/"
+     })
+     }
 
-
-
-}
+ }
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
